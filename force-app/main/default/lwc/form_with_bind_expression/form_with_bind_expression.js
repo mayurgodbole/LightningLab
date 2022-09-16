4c1231778d54da9374a3a8121aee47c9cf323d12
Mayur Godbole <mayurgodbole92@gmail.com>
2022-09-16 15:31:06 +0530

    Bind html element using Javascript

--- force-app/main/default/lwc/form_with_bind_expression/form_with_bind_expression.js
+++ force-app/main/default/lwc/form_with_bind_expression/form_with_bind_expression.js
@@ -1,5 +1,18 @@
 import { LightningElement } from 'lwc';
 
 export default class Form_with_bind_expression extends LightningElement {
+    handleChange(event){
+        const field = event.target.name;
+        if(field=='fullName'){
+            this.fullName = event.target.value;
+            
+        }
+        else if(field=='phone'){
+            this.phone == event.target.value;
+        }
+        else if(field=='email'){
+            this.email == event.target.value;
+        }
+    }
     
 }
