3103f9b760832149a4f40113cdc8f243e562e3a2
Mayur Godbole <mayurgodbole92@gmail.com>
2022-09-16 23:33:56 +0530

    Add Action(View,Edit,Delete) to each row

--- force-app/main/default/lwc/dataTableWithSearch/dataTableWithSearch.js
+++ force-app/main/default/lwc/dataTableWithSearch/dataTableWithSearch.js
@@ -1,15 +1,27 @@
 import { LightningElement } from 'lwc';
 import getData from '@salesforce/apex/ApexDatatable.getData';
 import { getReplicatedDataset } from 'lightning/analyticsWaveApi';
+const actions=[{label:'View', name:'View'},
+{label:'Edit', name:'Edit'},
+{label:'Delete', name:'Delete'}            
+];
 export default class DataTableWithSearch extends LightningElement {
 
     searchValue;
     displayResult;
+    
     columns=[
         {
             label: 'Name', fieldName:'Name'
+        },
+        {
+            type:'action',
+            typeAttributes:{
+                rowActions:actions,
+                menuAlighment:'right'
+            }
         }
-    ]
+    ];
     handleSearch(event){
         this.searchValue = event.target.value;
         this.ImperativeCall();
